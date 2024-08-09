b053a451abbb3f92cdb0fca3021b5f1bf52eb994
Mathansundar31 <mathankpr48@gmail.com>
2024-08-09 15:13:14 +0530

    file added ticket create

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const authMiddleware = require('../middleware/authMiddleware')
 
 const authRoutes = require('./authRoutes');
 const roleRoutes = require('./roleRoutes');
-const assignticketRoutes = require('./assignticketRoutes')
+const assignticketRoutes = require('./assignticketRoutes');
+const ticketcreateRoutes = require('./ticketcreateRoutes');
 
 app.use('/user',authRoutes)
 app.use('/role',roleRoutes)
 app.use('/ticket',assignticketRoutes)
+app.use('/ticket',ticketcreateRoutes)
 
 module.exports = app
