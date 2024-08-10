c9d4a5368ec7f3a005603058a810979140562f1c
Bharathamuthan <srbharath911@gmail.com>
2024-08-10 11:13:50 +0530

    folders add

--- utils/rolevalidation.js
+++ utils/rolevalidation.js
@@ -2,11 +2,13 @@ const Joi = require('joi');
 
 const roleSchema = Joi.object({
     name: Joi.string().min(5).max(30).required(),
+    role: Joi.string().min(2).max(30).required(),
     description: Joi.string().min(2).max(200).required(),
   });
 
   const roleupdateSchema = Joi.object({
     name: Joi.string().min(6).optional(),
+    role: Joi.string().min(6).optional(),
     description: Joi.string().optional(),
   });
 
