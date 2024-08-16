62b5a28894a3be30ea76f76670880ad0259194c5
Bharathamuthan <srbharath911@gmail.com>
2024-08-16 16:33:11 +0530

    index file

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const authMiddleware = require('../middleware/auth')
+const express = require('express');
+const auth = require('../middleware/auth');
+const app = express();
 
 const authRoutes = require('./authRoutes');
 const roleRoutes = require('./roleRoutes');
-const projectRoutes = require('./routes/projectRoutes');
-const submoduleRoutes = require('./routes/submoduleRoutes');
+const projectRoutes = require('./projectRoutes');
+const submoduleRoutes = require('./submoduleRoutes');
 const assignticketRoutes = require('./assignticketRoutes');
 const ticketcreateRoutes = require('./ticketcreateRoutes');
 
-app.use('/user',authRoutes)
-app.use('/role',roleRoutes)
+app.use(express.json());
+
+app.use('/user', authRoutes);
+app.use('/role', roleRoutes);
 app.use('/project', projectRoutes);
 app.use('/submodule', submoduleRoutes);
-app.use('/ticket',assignticketRoutes)
-app.use('/ticket',ticketcreateRoutes)
+app.use('/assignticket', assignticketRoutes);  
+app.use('/ticket', ticketcreateRoutes);
 
-module.exports = app
+module.exports = app;
