2d90589a3d22fdf5e60f7045d13ccd4cca9afff4
Bharathamuthan <srbharath911@gmail.com>
2024-08-16 11:25:39 +0530

    update for projectvalidation

--- utils/ProjectValidation.js
+++ utils/ProjectValidation.js
@@ -1,13 +1,8 @@
 const Joi = require('joi');
 
 const projectValidation = Joi.object({
-  projectresources: Joi.string().required(),
-  todo: Joi.string().required(),
-  qa: Joi.string().required(),
-  bugs: Joi.string().required(),
-  done: Joi.string().required(),
-  created_by: Joi.string().required(),
-  modify_by: Joi.string().required(), 
+  name: Joi.string().required(),
+  description: Joi.string().required(), 
 });
 
 module.exports = projectValidation;
